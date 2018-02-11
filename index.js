bfeb2300f74bfc026b44ce485e25a442b98219b9
Clint Taylor <stephen.c.taylor72@gmail.com>
2018-02-10 19:02:51 -0600

    add index.html css js files

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ var app=express();
 var bodyParser = require('body-parser');
 
 
-
-
-
 var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/',  function(req, res ){
-    res.send("Hello, from the root route");
+    res.sendFile("index.html");
 })
 
 app.use('/api/todos', todoRoutes);
